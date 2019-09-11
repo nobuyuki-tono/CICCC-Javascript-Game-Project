ab07d3026c1fa138b66fb940574bb84522248464
Nobu <rockmusic2090@gmail.com>
2019-09-11 16:39:38 -0700

    add animation to car

--- js/game.js
+++ js/game.js
@@ -3,6 +3,14 @@ var macarY = 0;
 var moveLeft = false;
 
 
+setTimeout(updat)
+
+function updateGame()
+{
+
+}
+
+
 function start(){
   document.getElementById("title").innerHTML="Welcome to 'Drive High Way'";
   document.getElementById("btn").innerHTML="Game Start";
@@ -14,6 +22,24 @@ function start(){
   
 }
 
+document.addEventListener('keyup', function (event) {
+  if (event.defaultPrevented) {
+      return;
+  }
+
+  var key = event.key || event.keyCode;
+
+  if (key === 'a' || key === 'A') {
+      moveleft();
+  }else if(key === 'd' || key === 'D'){
+    right();
+  }else if(key === 'w' || key === 'W'){
+    movetop();
+  }else if(key === 's' || key ==='S'){
+    movedown();
+  }
+});
+
 
 
 
@@ -21,6 +47,44 @@ function start(){
    
 function left(){
   // document.getElementById('mycar').style.left = 5;
-  document.getElementById("mycar").style.left = document.getElementById("mycar").style.left - 2 + "%";
-  console.log("working;")
+  var obj = document.getElementById("mycar");
+  var leftVal = parseInt(obj.style.left, 10);
+  obj.style.left = (leftVal - 2) + "%";
+  console.log("working;");
+}
+function right(){
+  // document.getElementById('mycar').style.left = 5;
+  var obj = document.getElementById("mycar");
+  var leftVal = parseInt(obj.style.left, 10);
+  obj.style.left = (leftVal + 2) + "%";
+  console.log("working;");
+}
+
+
+
+
+
+
+
+function moveleft(){
+  var obj = document.getElementById("mycar");
+  var leftVal = parseInt(obj.style.left, 10);
+  obj.style.left = (leftVal - 2) + "%";
+  console.log("working;");
+}
+function movetop(){
+  var obj = document.getElementById("mycar");
+  var topVal = parseInt(obj.style.bottom, 10);
+  obj.style.bottom = (topVal + 2) + "%";
+  console.log("working;");
+}
+function movedown(){
+  var obj = document.getElementById("mycar");
+  var topVal = parseInt(obj.style.bottom, 10);
+  obj.style.bottom = (topVal - 2) + "%";
+  console.log("working;");
+}
+
+function moveOtherCars(){
+
 }
