20ab66698cd270822b7ab0220418fc596b9b1d9a
Nobu <rockmusic2090@gmail.com>
2019-09-11 13:48:18 -0700

    added some code

--- js/game.js
+++ js/game.js
@@ -1,6 +1,26 @@
+var mycarX = 0;
+var macarY = 0;
+var moveLeft = false;
+
+
 function start(){
   document.getElementById("title").innerHTML="Welcome to 'Drive High Way'";
   document.getElementById("btn").innerHTML="Game Start";
+  document.getElementById("score").innerHTML="SCORE: 0";
+
+  document.getElementById("mycar").style.background.color = "red";
+
+  
+  
 }
 
 
+
+
+  
+   
+function left(){
+  // document.getElementById('mycar').style.left = 5;
+  document.getElementById("mycar").style.left = document.getElementById("mycar").style.left - 2 + "%";
+  console.log("working;")
+}
