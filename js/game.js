e26a8794094dbaa638e93feabe84fc7fa30403d9
Nobu <rockmusic2090@gmail.com>
2019-09-12 20:25:42 -0700

    added animation to other cars

--- js/game.js
+++ js/game.js
@@ -1,14 +1,14 @@
 var mycarX = 0;
 var macarY = 0;
-var moveLeft = false;
+
+var moveLeft = true;
+var moveRight = true;
+var moveDown = true;
+var moveUp = true;
 
 
-setTimeout(updat)
 
-function updateGame()
-{
 
-}
 
 
 function start(){
@@ -16,27 +16,45 @@ function start(){
   document.getElementById("btn").innerHTML="Game Start";
   document.getElementById("score").innerHTML="SCORE: 0";
 
-  document.getElementById("mycar").style.background.color = "red";
-
+ 
   
+  
+  moveOtherCars();
+  moveLine();
+  collision();
   
 }
 
-document.addEventListener('keyup', function (event) {
+
+
+document.addEventListener('keydown', function (event) {
   if (event.defaultPrevented) {
       return;
   }
 
-  var key = event.key || event.keyCode;
+  var key1 = event.key;
+  var key2 = event.keyCode;
 
-  if (key === 'a' || key === 'A') {
+  if (key1 === 'a' || key1 === 'A' || key2 == '37') {
+    if(moveLeft == true){
       moveleft();
-  }else if(key === 'd' || key === 'D'){
-    right();
-  }else if(key === 'w' || key === 'W'){
-    movetop();
-  }else if(key === 's' || key ==='S'){
-    movedown();
+    }
+      
+  }else if(key1 === 'd' || key1 === 'D' || key2 == '39'){
+    if(moveRight == true){
+      moveright();
+    }
+    
+  }else if(key1 === 'w' || key1 === 'W' | key2 == '38'){
+    if(moveUp == true){
+      moveup();
+    }
+    
+  }else if(key1 === 's' || key1 ==='S'| key2 == '40'){
+    if(moveDown == true){
+      movedown();
+    }
+    
   }
 });
 
@@ -45,46 +63,254 @@ document.addEventListener('keyup', function (event) {
 
   
    
-function left(){
+
+
+
+function moveright(){
   // document.getElementById('mycar').style.left = 5;
   var obj = document.getElementById("mycar");
   var leftVal = parseInt(obj.style.left, 10);
-  obj.style.left = (leftVal - 2) + "%";
-  console.log("working;");
+  obj.style.left = (leftVal + 5) + "%";
+
+  var gamearea = document.getElementById("gamearea");
+  var gameareawidth = window.getComputedStyle(gamearea,null).getPropertyValue("width");
+  
+
+  var mycarX = document.getElementById("mycar");
+  var mycarLeft = window.getComputedStyle(mycarX,null).getPropertyValue("left");
+
+  
+
+  if(mycarLeft != 0+ "px"){
+    moveLeft = true;
+  }
+
+  if(mycarLeft == 266.688 + "px"){
+    moveRight = false;
+  }
+
 }
-function right(){
-  // document.getElementById('mycar').style.left = 5;
-  var obj = document.getElementById("mycar");
-  var leftVal = parseInt(obj.style.left, 10);
-  obj.style.left = (leftVal + 2) + "%";
-  console.log("working;");
-}
-
-
-
-
-
 
 
 function moveleft(){
   var obj = document.getElementById("mycar");
   var leftVal = parseInt(obj.style.left, 10);
-  obj.style.left = (leftVal - 2) + "%";
-  console.log("working;");
+  obj.style.left = (leftVal - 5) + "%";
+
+  var gamearea = document.getElementById("gamearea");
+  var gameareawidth = window.getComputedStyle(gamearea,null).getPropertyValue("width");
+
+  
+
+  var mycarX = document.getElementById("mycar");
+  var mycarLeft = window.getComputedStyle(mycarX,null).getPropertyValue("left");
+
+  
+
+  if(mycarLeft < 265 + "px"){
+    moveRight = true;
+  }
+
+  if(mycarLeft == 0+"px"){
+    moveLeft = false;
+  }
+
+  
+
 }
-function movetop(){
+
+
+function moveup(){
   var obj = document.getElementById("mycar");
   var topVal = parseInt(obj.style.bottom, 10);
-  obj.style.bottom = (topVal + 2) + "%";
-  console.log("working;");
+  obj.style.bottom = (topVal + 5) + "%";
+
+  var gamearea = document.getElementById("gamearea");
+  var gameareawidth = window.getComputedStyle(gamearea,null).getPropertyValue("height");
+
+  
+  var mycarX = document.getElementById("mycar");
+  var mycarTop = window.getComputedStyle(mycarX,null).getPropertyValue("bottom");
+
+  
+
+  if(mycarTop == 430 + "px"){
+    moveUp = false;
+  }
+
+  if(mycarTop != 435 + "px"){
+    moveDown = true;
+  }
 }
 function movedown(){
   var obj = document.getElementById("mycar");
   var topVal = parseInt(obj.style.bottom, 10);
-  obj.style.bottom = (topVal - 2) + "%";
-  console.log("working;");
+  obj.style.bottom = (topVal - 5) + "%";
+
+  
+  
+  // get css property
+  var gamearea = document.getElementById("gamearea");
+  var gameareawidth = window.getComputedStyle(gamearea,null).getPropertyValue("height");
+
+ 
+
+  var mycarX = document.getElementById("mycar");
+  var mycarTop = window.getComputedStyle(mycarX,null).getPropertyValue("top");
+
+  
+
+  if(mycarTop == 435+ "px"){
+    moveDown = false;
+  }
+
+  if(mycarTop != 10 + "px"){
+    moveUp = true;
+  }
 }
+
+
 
 function moveOtherCars(){
+  var obj1 = document.getElementById("car-1");
+  var topVal = parseInt(obj1.style.top, 10);
+  obj1.style.top = (topVal + 4) + "px";
+  
+  var obj2 = document.getElementById("car-2");
+  var topVal = parseInt(obj2.style.top, 10);
+  obj2.style.top = (topVal + 4) + "px";
+  
+  var obj3 = document.getElementById("car-3");
+  var topVal = parseInt(obj3.style.top, 10);
+  obj3.style.top = (topVal + 4) + "px";
+
+var car1 = document.getElementById("car-1");
+var carHeight = window.getComputedStyle(car1,null).getPropertyValue("top");
+var car2 = document.getElementById("car-2");
+var carHeight2 = window.getComputedStyle(car2,null).getPropertyValue("top");
+var car3 = document.getElementById("car-3");
+var carHeight3 = window.getComputedStyle(car3,null).getPropertyValue("top");
+
+
+var gamearea = document.getElementById("gamearea");
+var gameareaHeight = window.getComputedStyle(gamearea,null).getPropertyValue("height");
+//console.log(gameareaHeight);
+
+  
+  //console.log(carHeight);
+
+  if(carHeight == gameareaHeight){
+    // console.log("working");
+    var obj1 = document.getElementById("car-1");
+    var topVal = parseInt(obj1.style.top, 10);
+    obj1.style.top = -200 + "px";
+  }
+
+  if(carHeight2 == gameareaHeight){
+    var obj2 = document.getElementById("car-2");
+    var topVal = parseInt(obj2.style.top, 10);
+    obj2.style.top = -400 + "px";
+  }
+
+  if(carHeight3 == gameareaHeight){
+    var obj3 = document.getElementById("car-3");
+    var topVal = parseInt(obj3.style.top, 10);
+    obj3.style.top = -300 + "px";
+  }
+  
+  
+}
+
+
+
+function moveLine(){
+  var line1 = document.getElementById("line1");
+  var topVal = parseInt(line1.style.top, 10);
+  line1.style.top = (topVal + 4) + "px";
+  
+  var line2 = document.getElementById("line2");
+  var topVal = parseInt(line2.style.top, 10);
+  line2.style.top = (topVal + 4) + "px";
+ 
+  var line3 = document.getElementById("line3");
+  var topVal = parseInt(line3.style.top, 10);
+  line3.style.top = (topVal + 4) + "px";
+
+
+  
+  // repeat line
+  var linerepeat3 = document.getElementById("line3");
+  var lineHeight3 = window.getComputedStyle(linerepeat3,null).getPropertyValue("top");
+
+  
+  
+  var linerepeat2 = document.getElementById("line2");
+  var lineHeight2 = window.getComputedStyle(linerepeat2,null).getPropertyValue("top");
+
+  
+  
+  var linerepeat1 = document.getElementById("line1");
+  var lineHeight1 = window.getComputedStyle(linerepeat1,null).getPropertyValue("top");
+
+  
+
+  if(lineHeight3 == 550 + "px"){
+    var line3 = document.getElementById("line3");
+    var topVal = parseInt(line3.style.top, 10);
+    line3.style.top = -450 + "px";
+    
+    //console.log(gameareaHeight)
+    
+  }
+  if(lineHeight2 == 550 + "px"){
+    var line2 = document.getElementById("line2");
+    var topVal = parseInt(line2.style.top, 10);
+    line2.style.top = -450 + "px";
+  
+    //console.log(gameareaHeight)
+    
+  }
+  if(lineHeight1 == 550 + "px"){
+    var line1 = document.getElementById("line1");
+    var topVal = parseInt(line1.style.top, 10);
+    line1.style.top = -450 + "px";
+   
+    //console.log(gameareaHeight)
+   
+  }
 
 }
+
+function collision(){
+  // get css property
+    var obj1 = document.getElementById("car-1");
+    var otherCars1Left = window.getComputedStyle(obj1,null).getPropertyValue("left");
+    var otherCars1Top = window.getComputedStyle(obj1,null).getPropertyValue("top");
+
+  // get my car css property
+  var mycarX = document.getElementById("mycar");
+  var mycarTop = window.getComputedStyle(mycarX,null).getPropertyValue("top");
+  var mycarLeft = window.getComputedStyle(mycarX,null).getPropertyValue("left");
+    
+  var mycarNumtop = parseInt(mycarTop);
+  var othercarsNumtop = parseInt(otherCars1Top);
+  var topgap = mycarNumtop - othercarsNumtop;
+
+  var mycarNumLeft = parseInt(mycarLeft);
+  var otherCarsNumLeft = parseInt(otherCars1Left);
+  var gapLeft1 = mycarNumLeft - otherCarsNumLeft;
+  var gapLeft2 = otherCarsNumLeft - mycarNumLeft;
+   
+    console.log(mycarNumLeft);
+    console.log(otherCarsNumLeft);
+
+
+    // console.log(mycarLeft);
+
+    if(topgap < 40 && ((gapLeft2 < 40 && 0 <= gapLeft2)||(gapLeft1 > -50 && 0 >= gapLeft1))){
+      console.log("Clash");
+    }
+
+   
+}
+
